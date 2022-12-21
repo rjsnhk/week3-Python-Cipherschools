aa0c3d7c1abf5272943a3afe7c7384905d73017b
Rajesh Nahak <114793887+rjsnhk@users.noreply.github.com>
2022-12-22 00:00:37 +0530

    Add files via upload

---
+++ class129-intro.to.sets-cipherschools.py
@@ -0,0 +1,36 @@
+# set contains all type of value--num,float,string
+# but list dictionary store nhi kr skte
+
+# koi sequence me print nhi hoga apni marji se hoga
+
+# s={1,2,3,4,2,3} #unique items only print not repeating
+# print(s)  #{1, 2, 3, 4}
+
+
+
+# l=[1,1,2,2,3,4,4,23,2,1,1,1,3,3,34]
+# s2=set(l)
+# print(list(s2))  #[1, 2, 3, 4, 34, 23]
+
+# a={1,2,3,4}
+# a.add(23)
+# a.add(200)
+# a.remove(3)
+# a.discard(1)
+# # a.remove(9) erroe ocure
+# a.discard(9)   #no error
+# print(a) #{1, 2, 4, 200, 23}
+
+
+# a={1,2,3,4}
+# # a.clear()
+# # print(a)
+
+# z=a.copy()
+# print(z)
+
+# z.discard(3)
+# print(z)
+
+s={1,1.2,2.3,'hari'}
+print(s)

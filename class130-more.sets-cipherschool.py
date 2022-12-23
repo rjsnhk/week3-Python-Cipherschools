6294f784143b26429a33520402931c496c8c6a83
Rajesh Nahak <114793887+rjsnhk@users.noreply.github.com>
2022-12-23 23:32:15 +0530

    Add files via upload

---
+++ class130-more.sets-cipherschool.py
@@ -0,0 +1,21 @@
+s={'a','b','c'}
+# if 'a'in s:
+#     print('present')
+# else:
+#     print('not present')
+
+# for i in s:
+#     print(i) #random order ayega
+    
+    
+# set maths
+s1={1,2,3,4,5}
+s2={4,5,6,7,8,9}
+
+# # union={1,2,3,4,5,6,7,8,9}
+# union_set= s1 | s2
+# print(union_set)
+
+# intersection={4,5}
+intersection= s1 & s2
+print(intersection)

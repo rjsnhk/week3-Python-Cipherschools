61fea938502c775a5ddcf57ed58f927d480f8b72
Rajesh Nahak <114793887+rjsnhk@users.noreply.github.com>
2022-12-25 22:58:59 +0530

    Add files via upload

---
+++ class131-what.is.list-cipherschool.py
@@ -0,0 +1,24 @@
+# list creat
+# square=[]
+# for i in range(1,10):
+#     square.append(i**2)
+# print(square)
+
+# list
+# sq2=[i**2 for i in range(1,10)]
+# print(sq)
+
+# sq3=[-i for i in range(1,10)]
+# print(sq3)
+
+
+# names=['rajesh','nahak','ram','baba']
+# k=[]
+# for name in names:
+#     k.append(name[0])
+# print(k)
+
+# list is
+names=['rajesh','nahak','ram','baba']
+n1=[name[0] for name in names]
+print(n1)

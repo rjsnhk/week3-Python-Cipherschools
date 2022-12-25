b35826ca2f93836c6efa3dd66cf380b4754a6e83
Rajesh Nahak <114793887+rjsnhk@users.noreply.github.com>
2022-12-26 02:44:23 +0530

    Add files via upload

---
+++ class138-nested.list.comprehension-cipherschool.py
@@ -0,0 +1,11 @@
+# example=[[1,2,3],[1,2,3],[4,5,6]]
+k=[]
+for i in range(3):
+    k.append([1,2,3])
+print(k)
+
+
+# # string compre
+# x=[[(i) for i in range(1,4)]for j in range(3)]
+
+# print(x)

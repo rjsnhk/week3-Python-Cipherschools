9bcb041702fc7390ee551fff58d5608960d799fc
Rajesh Nahak <114793887+rjsnhk@users.noreply.github.com>
2023-01-08 22:01:36 +0530

    Add files via upload

---
+++ class149-func.with.alltype.parameter-cipherschool.py
@@ -0,0 +1,25 @@
+# parameter
+# def func(name):
+#     print(name)
+    
+# n='raj'
+# func(n)
+
+# default parameter
+# def func2(name='unknown',age=24):
+#     print(name)
+#     print(age)
+# n='raj'
+# a=19
+# # func2()
+# func2(n,a)
+
+# def func3(name,*args,lname='unknown',**kwargs):
+#     print(name)
+#     print(args)
+#     print(lname)
+#     print(kwargs)
+    
+# func3('raj',1,2,3,n='raj',age=24)
+
+# use series padk

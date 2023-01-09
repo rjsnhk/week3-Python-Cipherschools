f62cf4a5a4dee3b1e9f8807a0174502e06f48bb4
Rajesh Nahak <114793887+rjsnhk@users.noreply.github.com>
2023-01-09 19:26:03 +0530

    Add files via upload

---
+++ class159-zip.func.part1-cipherschool.py
@@ -0,0 +1,26 @@
+# user=['use1','use2','use3']
+# name=['raj','ram','har']
+
+# # zip function
+# mix=zip(user,name)  #iter
+# print(list(mix))
+# # out--[('use1', 'raj'), ('use2', 'ram'), ('use3', 'har')]
+
+
+# user=['use1','use2']
+# name=['raj','ram','har']
+# mix=zip(user,name)  #iter
+# print(list(mix)) #out---[('use1', 'raj'), ('use2', 'ram')]
+
+
+# user=['use1','use2']
+# name=['raj','ram']
+# mix=zip(user,name)  #iter
+# print(dict(mix)) #out--{'use1': 'raj', 'use2': 'ram'}
+
+user=['use1','use2']
+fname=['raj','ram']
+lname=['nahak','sahu']
+mix=zip(user,fname,lname)  #iter
+# print(list(mix))
+# print(dict(mix)) --not convert

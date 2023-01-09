cd88cceecb51378ac89a82dfff5e074538c38944
Rajesh Nahak <114793887+rjsnhk@users.noreply.github.com>
2023-01-09 16:33:18 +0530

    Add files via upload

---
+++ class156-map.function-cipherschool.py
@@ -0,0 +1,29 @@
+num=[1,2,3,4,]
+
+# output [1,4,9,16]
+
+# l=[]
+# for i in num:
+#     l.append(i**2)
+# print(l)
+
+# squr=[i**2 for i in num]
+# print(squr)
+
+# ###### maap function
+# sqr=list(map(lambda a:a**2,num))
+# print(sqr)
+
+s=['abs','asdf','asdfg']
+
+# length=[len(i) for i in s]
+# print(length)
+
+# l=[]
+# for i in s:
+#     l.append(len(i))
+# print(l)
+
+m=list(map(len,s))
+print(m)
+

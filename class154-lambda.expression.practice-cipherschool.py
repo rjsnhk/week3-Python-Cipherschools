030bd7d5d38c9153156c84f08b9db794f8a1179a
Rajesh Nahak <114793887+rjsnhk@users.noreply.github.com>
2023-01-09 16:32:56 +0530

    Add files via upload

---
+++ class154-lambda.expression.practice-cipherschool.py
@@ -0,0 +1,32 @@
+# lambda
+# def even(a):
+#     if a%2==0:
+#         return True
+#     else:
+#         return False
+    
+# print(even(6))
+
+
+# def even(a):
+#     return a%2==0
+# print(even(6))
+
+# even2=lambda a:a%2==0
+# print(even2(9))
+
+
+# # last character
+# last=lambda x:x[-1]
+# print(last('rajes'))  #s
+
+# # lambda with if else
+# func=lambda s: True if len(s)>5 else False
+
+# print(func('rajes')) #False
+
+
+# lambda with if else
+func2=lambda s:len(s)>5
+
+print(func2('rajesh')) #False
